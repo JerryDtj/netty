155e79242a8d0d0be048ba4272f2743fb42439df
邓天骄 <jiao_snow@126.com>
2019-10-05 12:24:37 +0800

    第一个netty项目:编写一个消息丢弃server

---
+++ discardServer/src/main/java/DiscardServerHandle.java
@@ -0,0 +1,11 @@
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author Jerry
+ * @Date 2019/10/5 12:05 下午
+ */
+public class DiscardServerHandle extends ChannelInboundHandlerAdapter {
+
+
+}
